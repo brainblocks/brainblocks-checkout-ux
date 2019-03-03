9404f74839b4b785e8c3027277cdceb2db10e828
Daniel Brain <daniel@brainblocks.io>
2019-03-02 16:31:16 -0800

    Add global styles

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -18,11 +18,22 @@ const Checkout = () => {
     return (
         <div>
             <Head>
+                <link href="https://fonts.googleapis.com/css?family=Maven+Pro:700,900|Montserrat:400,600|Source+Code+Pro:600" rel="stylesheet" />
                 <script>
                     window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = window.parent.__REACT_DEVTOOLS_GLOBAL_HOOK__;
                 </script>
             </Head>
-            
+
+            <style jsx global>
+                {`
+                    html, body {
+                        margin: 0;
+                        padding: 0;
+                        font-family: Montserrat,sans-serif;
+                    }
+                `}
+            </style>
+
             { brainblocksScript }
 
             <style jsx>{`

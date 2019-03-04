1086b1eb37826cf708a79924093a21877b91a127
Daniel Brain <daniel@brainblocks.io>
2019-03-03 18:20:53 -0800

    Show spinner on button while checkout is rendering

--- pages/button.jsx
+++ pages/button.jsx
@@ -1,30 +1,50 @@
 /* @flow */
 
-import React from 'react';
+import React, { useState } from 'react';
+import Head from 'next/head';
 
 import { useBrainBlocksScript } from '../hooks';
 import BrainBlocksLogo from '../img/brainblocks_logo.svg';
+import { Spinner } from '../components/spinner';
 
 const Button = () => {
     const { brainblocks, brainblocksScript } = useBrainBlocksScript();
+    const [ loading, setLoading ] = useState(false);
 
     const onClick = () => {
         if (!brainblocks) {
             throw new Error(`BrainBlocks script not loaded`);
         }
 
+        setLoading(true);
+
         return brainblocks.Checkout({
             env:         window.xprops.env,
             transaction: window.xprops.transaction,
             onComplete:  window.xprops.onComplete
-        }).renderTo(window.top, 'body');
+        }).renderTo(window.top, 'body').then(() => {
+            setLoading(false);
+        });
     };
 
     return (
         <div>
+            <Head>
+                <link href="https://fonts.googleapis.com/css?family=Maven+Pro:700,900|Montserrat:400,600|Source+Code+Pro:600" rel="stylesheet" />
+                <script>
+                    window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = window.parent.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+                </script>
+            </Head>
+
             { brainblocksScript }
 
             <style jsx global>{`
+                * {
+                    font-family: Montserrat,sans-serif;
+                    box-sizing: border-box;
+                    user-select: none;
+                }
+        
                 html, body { 
                     margin: 0;
                     padding: 0;
@@ -50,10 +70,17 @@ const Button = () => {
                     cursor: pointer;
                     font-family: helvetica, sans-serif;
                     text-align: center;
+                    position: relative;
+                    color: #333;
+                }
+
+                button::focus {
+                    color: #333;
                 }
 
                 button .logo {
                     height: 50vh;
+                    visibility: ${ loading ? 'hidden' : 'visible' }
                 }
 
                 button .logo .text {
@@ -62,6 +89,15 @@ const Button = () => {
                     margin-top: 2px;
                     margin-right: 5px;
                     letter-spacing: 1px;
+                }
+
+                button .spinner {
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    height: 25px;
+                    width: 25px;
                 }
 
                 @media only screen and (min-width: 100px) {
@@ -117,6 +153,7 @@ const Button = () => {
 
             <button data-testid="checkout-button" onClick={ onClick } type='button'>
                 <div className="logo"><span className="text">Pay with</span><BrainBlocksLogo /></div>
+                { loading && <div className='spinner'><Spinner /></div> }
             </button>
         </div>
     );
